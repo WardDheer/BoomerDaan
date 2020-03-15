a68c8982894a14f187a8d571b6e90067d2509ff3
Maxim Eylenbosch <55977182+MaximEylenbosch@users.noreply.github.com>
2020-03-15 18:45:21 +0100

    ADDED: Card mechanic and some props

---
+++ project/Assets/Scripts/Card.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    private bool _selected = false;
+
+    public void OnMouseEnter()
+    {
+        if (!_selected)
+        {
+            LeanTween.moveY(this.gameObject, 200, 0.25f);
+        }
+    }
+
+    public void OnMouseExit()
+    {
+        if (!_selected)
+        {
+            LeanTween.moveY(this.gameObject, 125, 0.5f);
+        }
+    }
+
+    public void OnMouseClick()
+    {
+        if (!_selected)
+        {
+            _selected = true;
+            LeanTween.moveY(this.gameObject, 200, 0.25f);
+        }
+        else
+        {
+            _selected = false;
+            LeanTween.moveY(this.gameObject, 125, 0.5f);
+        }
+    }
+}
---
+++ project/Assets/Scripts/CardController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardController : MonoBehaviour
+{
+    [SerializeField] private GameObject _cardHolder;
+    private FirstPersonAIO _player;
+
+    private void Start()
+    {
+        _player = FindObjectOfType<FirstPersonAIO>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("In");
+            //animatie, kaarten sliden in
+            LeanTween.moveY(_cardHolder, 125, 0.5f);
+            
+            _player.lockAndHideCursor = false;
+            _player.enableCameraMovement = false;
+            Cursor.visible = true;
+            
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Out");
+            //animatie, kaarten sliden uit
+            LeanTween.moveY(_cardHolder, -125, 0.5f);
+            
+            _player.enableCameraMovement = true;
+            _player.lockAndHideCursor = true;
+
+            Cursor.visible = false;
+        }
+    }
+}
