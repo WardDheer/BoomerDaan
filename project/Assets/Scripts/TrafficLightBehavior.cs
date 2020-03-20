becb40045f28caebcca0458d44f9614f693a8371
BriekKeters <briek.keters@student.howest.be>
2020-03-20 10:48:12 +0100

    Changed Traffic light added some textures

--- project/Assets/Scripts/TrafficLightBehavior.cs
+++ project/Assets/Scripts/TrafficLightBehavior.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class TrafficLightBehavior : MonoBehaviour
 {
+    public enum Light {Red, Green, Orange}
     public Material MaterialRedLight;
     public Material MaterialGreenLight;
     public Material MaterialOrangeLight;
     public Material MaterialBlackLight;
+    public Light currentLight;
 
     private float _time;
     private bool _isOrange,_isRed = false;
+    private bool _isGreen = true;
     [SerializeField]private float _redTimer;
     [SerializeField]private float _orangeTimer;
+    [SerializeField]private float _greenTimer;
     [SerializeField]private MeshRenderer _renderer;
     [SerializeField]private Material[] _currentLightMaterials = new Material[5];
 
@@ -27,11 +31,13 @@ public class TrafficLightBehavior : MonoBehaviour
     {
         if(_isOrange)
         {
+            currentLight = Light.Orange;
             _time += Time.deltaTime;
             if(_time>_orangeTimer)
             {
-            Debug.Log("Change");
+            Debug.Log("Red");
             _currentLightMaterials[3] = MaterialBlackLight;
+            _currentLightMaterials[4] = MaterialBlackLight;
             _currentLightMaterials[2]= MaterialRedLight;
             _renderer.materials = _currentLightMaterials;
             _isRed = true;
@@ -41,14 +47,33 @@ public class TrafficLightBehavior : MonoBehaviour
         }
         if(_isRed)
         {
+            currentLight = Light.Red;
             _time += Time.deltaTime;
             if(_time>_redTimer)
             {
-            Debug.Log("Change");
+            Debug.Log("Green");
             _currentLightMaterials[2] = MaterialBlackLight;
+            _currentLightMaterials[3] = MaterialBlackLight;
             _currentLightMaterials[4]= MaterialGreenLight;
             _renderer.materials = _currentLightMaterials;
             _isRed = false;
+            _isGreen = true;
+            _time =0;
+            }
+        }
+        if(_isGreen)
+        {
+            currentLight = Light.Green;
+            _time += Time.deltaTime;
+            if(_time>_greenTimer)
+            {
+            Debug.Log("Orange");
+            _currentLightMaterials[4] = MaterialBlackLight;
+            _currentLightMaterials[2] = MaterialBlackLight;
+            _currentLightMaterials[3]= MaterialOrangeLight;
+            _renderer.materials = _currentLightMaterials;
+            _isGreen = false;
+            _isOrange = true;
             _time =0;
             }
         }
@@ -56,13 +81,13 @@ public class TrafficLightBehavior : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag =="Player")
-        {
-            _currentLightMaterials[4] = MaterialBlackLight;
-            _currentLightMaterials[3] = MaterialOrangeLight;
-            _currentLightMaterials[2] = MaterialBlackLight;
-            _isOrange = true;
-            _renderer.materials = _currentLightMaterials;
-        }
+        // if(other.tag =="Player")
+        // {
+        //     _currentLightMaterials[4] = MaterialBlackLight;
+        //     _currentLightMaterials[3] = MaterialOrangeLight;
+        //     _currentLightMaterials[2] = MaterialBlackLight;
+        //     _isOrange = true;
+        //     _renderer.materials = _currentLightMaterials;
+        // }
     }
 }
